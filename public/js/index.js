c75807e8924190cdc4676468dcd47a200f90b6aa
2k4pxthanh <2k4pxthanh@gmail.com>
2024-07-06 19:06:19 +0700

    fix web

--- public/js/index.js
+++ public/js/index.js
@@ -12,8 +12,11 @@ myVideo.muted = true;
 const peers = {};
 const videoElements = {}; // Đối tượng lưu trữ các phần tử video
 
+const web = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+
 // Yêu cầu quyền truy cập vào video và audio của người dùng
-navigator.mediaDevices.getUserMedia({
+web.mediaDevices.getUserMedia({
     video: true,
     audio: true
 }).then(stream => {
